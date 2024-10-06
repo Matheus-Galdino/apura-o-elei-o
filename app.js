15fc826d224dddd0cf838a1609694a1636a00876
Matheus Henrique Galdino Carvalho <matheus.galdino@ed.dev.br>
2024-10-06 17:46:27 -0300

    atualização eleições 2024

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const numberFormater = new Intl.NumberFormat();
-const ENDPOINT = "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json";
+const ENDPOINT = "https://resultados.tse.jus.br/oficial/ele2024/619/dados/sp/sp71072-c0011-e000619-u.json";
 
 const candidatosList = document.querySelector(".candidatos");
 const atualizadoEm = document.querySelector(".atualizado-em");
@@ -7,25 +7,27 @@ const atualizadoEm = document.querySelector(".atualizado-em");
 ListarResultados();
 
 setInterval(async () => {
-    await ListarResultados()
-}, 1 * 60000);
+  await ListarResultados()
+}, 0.5 * 60000);
 
 async function ListarResultados() {
   const response = await fetch(ENDPOINT);
   const result = await response.json();
 
+  const candidatos = result.carg[0].agr.map(agr => agr.par[0].cand[0])
+  const candidatosOrdenado = candidatos.sort((x, y) => x.vap == y.vap ? 0 : Number(x.vap) > Number(y.vap) ? -1 : 1)
+  console.log(candidatosOrdenado)
+  
   candidatosList.innerHTML = "";
-  for (const cand of result.cand) {
-    candidatosList.innerHTML += CreateCandidatoListItem(cand);
-  }
+  candidatosList.innerHTML += candidatosOrdenado.reduce((html, cand) => html += CreateCandidatoListItem(cand), "");
 
-  atualizadoEm.innerHTML = "Atualizado em: " + new Date().toLocaleTimeString();
+  atualizadoEm.innerHTML = `Atualizado em: <span> ${new Date().toLocaleTimeString()} </span>`;
 }
 
 function CreateCandidatoListItem(candidato) {
   return `
     <li class="candidato">
-        <p class="candidato-nome">${candidato.nm}</p>
+        <p class="candidato-nome">${candidato.n}-${candidato.nmu}</p>
         <p class="candidato-votos">
             Votos totais: 
             <strong> ${numberFormater.format(candidato.vap)} </strong>
